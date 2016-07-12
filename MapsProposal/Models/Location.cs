6fd0980a1f5631dc83f71f7e04896d5cb26cab1c
Jeremy Bennett <jeremy.bennett@gbmmec.com>
2016-07-12 15:46:43 +0100

    added dbcontext

---
+++ MapsProposal/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapsProposal.Models
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
+    }
+}
